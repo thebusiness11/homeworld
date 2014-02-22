84fd10ba5c7acb0e35cf9193ef0530455b74756a
Lance Velasco <lancevelasco@lances-Mac-mini.local>
2014-02-22 13:44:13 -0800

    initial commit to azure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homeworld::Application.config.secret_key_base = '824b0283288f25b84ded257570f5de4a6b9e660eec0d856bfdafe5ce0dc043607635cd4d41f1f2aef0deead0d2f78c9d4ec226d62b8f57f6ede8fbddd29c6394'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Homeworld::Application.config.session_store :cookie_store, key: '_homeworld_session'
